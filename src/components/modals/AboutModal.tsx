aae987f7e1d3a8724b32b578f5267b1c199e047c
Chithanh123 <ctvtskg@gmail.com>
2022-02-04 21:30:52 +0700

    commit

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="về game" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Về game" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500">
         Đây là phiên bản mã nguồn mở của tựa game Wordle xây dựng cho{' '}
         {CONFIG.language} bởi{' '}
